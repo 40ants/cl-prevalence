99440c4fff7421dcd85a96f46adfdd44f75d6da7
Pierre Neidhardt <mail@ambrevar.xyz>
2022-11-22 10:26:27 +0100

    Do not trigger error when deserializing non-existing slot

--- src/serialization/sexp.lisp
+++ src/serialization/sexp.lisp
@@ -260,12 +260,13 @@ second element its value."))
                                    (mapcar
                                     (lambda (slot-name+value)
                                       (let* ((slot-name (first slot-name+value))
-                                             (slot-val (rest slot-name+value))
-                                             (initarg (first (getf (mopu:slot-properties class-symbol slot-name) :initargs))))
-                                        (if initarg
-                                            (list initarg
-                                                  (deserialize-sexp-slot (find-class class-symbol) slot-name slot-val deserialized-objects))
-                                            (push slot-name+value no-initarg-slots))))
+                                             (slot-val (rest slot-name+value)))
+                                        (when (find slot-name (mopu:slot-names class-symbol))
+                                          (let ((initarg (first (getf (mopu:slot-properties class-symbol slot-name) :initargs))))
+                                            (if initarg
+                                                (list initarg
+                                                      (deserialize-sexp-slot (find-class class-symbol) slot-name slot-val deserialized-objects))
+                                                (push slot-name+value no-initarg-slots))))))
                                     slots))))))
     (dolist (slot-name+value no-initarg-slots)
       (let ((slot-name (first slot-name+value))
